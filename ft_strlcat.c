7ee07aea8b2c1b3738251f5617b51c58f02a9e16
Maxime Creus <mcreus@c2r3p1.42perpignan.fr>
2023-02-03 17:59:12 +0100

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 15:29:22 by mcreus            #+#    #+#             */
+/*   Updated: 2023/02/03 17:55:53 by mcreus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = strlen(dest);
+	src_len = strlen(src);
+	if (size <= dest_len)
+		return (src_len + size);
+	i = 0;
+	while (scr[i] && ((dest_len + i) < (size - 1)))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
