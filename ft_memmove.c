3a51b68cfa9688e98a31072734256ed84f6b61b6
Maxime Creus <mcreus@c2r3p1.42perpignan.fr>
2023-02-03 15:36:52 +0100

    premier envoi secu

---
+++ ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 15:00:26 by mcreus            #+#    #+#             */
+/*   Updated: 2023/02/03 14:45:13 by mcreus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	if (!dest || !src)
+		return (NULL);
+	if (dest > src)
+	{
+		i = (int)n - 1;
+		while (i >= 0)
+		{
+			*(char *)(dest + i) = *(char *)(src + i);
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < (int)n)
+		{
+			*(char *)(dest + i) = *(char *)(src + i);
+			i++;
+		}
+	}
+	return (dest);
+}
