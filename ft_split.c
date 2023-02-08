7f215c17a3a3c86a18cc784ef562dc39430e92e7
melmut66 <124189787+melmut66@users.noreply.github.com>
2023-02-08 19:31:28 +0100

    Add files via upload

---
+++ ft_split.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 09:58:21 by mcreus            #+#    #+#             */
+/*   Updated: 2023/02/08 16:54:06 by mcreus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_words(const char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		while (str[i] != c && str[i] != '\0')
+			count++;
+			i++;
+	}
+	return (count);
+}
+
+static char	*ft_word_dup(const char *str, int start, int end)
+{
+	int		i;
+	char	*word;
+
+	i = 0;
+	word = (char *)malloc(sizeof(char) * (end - start + 1));
+	while (start < end)
+	{
+		word[i] = str[start];
+		start++;
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		index;
+	char	**split;
+
+	split = (char **)malloc(sizeof(char) * (ft_count_words(*s, c) + 1));
+	if (!s || !split)
+		return (NULL);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if (s[i] == c || i == ft_strlen(s) && index >= 0)
+		{
+			split[j] = ft_word_dup (s, index, i);
+			f++;
+			index -= 1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
+}
